2e6d6a320cca125113dfb7d77237adeb12a1c537
kielmariano38 <kielmariano38@gmail.com>
2026-03-13 20:38:18 +0800

    Add files via upload

---
+++ netlify/functions/meters.js
@@ -0,0 +1,51 @@
+let meters = {
+  boxingKing: {
+    value: 60,
+    increasing: true
+  },
+  goldenEmpire: {
+    value: 45,
+    increasing: false
+  }
+};
+
+function updateMeter(meter){
+
+  const step = Math.random()*(0.8-0.05)+0.05;
+
+  if(meter.increasing){
+
+    meter.value += step;
+
+    if(meter.value >= 90){
+      meter.value = 90;
+      meter.increasing = false;
+    }
+
+  }else{
+
+    meter.value -= step;
+
+    if(meter.value <= 10){
+      meter.value = 10;
+      meter.increasing = true;
+    }
+
+  }
+
+}
+
+exports.handler = async function(){
+
+  updateMeter(meters.boxingKing);
+  updateMeter(meters.goldenEmpire);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      boxingKing: meters.boxingKing.value.toFixed(2),
+      goldenEmpire: meters.goldenEmpire.value.toFixed(2)
+    })
+  };
+
+};
